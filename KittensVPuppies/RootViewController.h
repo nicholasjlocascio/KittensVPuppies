8a988d10223ad3333f7f0555d890f9028f1a70b9
Nicholas Locascio <champ6926@gmail.com>
2011-12-07 11:53:21 -0500

    Initial Commit

---
+++ KittensVPuppies/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  KittensVPuppies
+//
+//  Created by Nicholas Locascio on 12/7/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
